d65a40a3c45184fc1e4ba1f31bd8680851a81ba4
Linmo <30459943+MoorCorPa@users.noreply.github.com>
2022-08-24 21:10:08 +0800

    修改主页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,16 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme, defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '一个为实现梦想而奋斗的Minecraft制作组',
-  description: '~',
+  title: 'Ruler14',
+  description: '一个为实现梦想而奋斗的Minecraft制作组',
   base: 'Ruler14Group',
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: '关于',
+        link: '/about'
+      }
+    ]
+  })
 })
--- docs/index.md
+++ docs/index.md
@@ -1,12 +1,10 @@
 ---
 lang: zh-CN
 title: Ruler14
-navbar: false
-sidebar: false
+home: true
+heroImage: https://p.qlogo.cn/gh/776863832/776863832/0
 ---
 
 <div style="text-align: center">
-    <h1 style="padding: 20px">我们是 Ruler14</h1>
-    <h2>一个为实现梦想而奋斗的Minecraft制作组</h2>
     <p>“君若知遥，此道方长。意投则迎，执见亦欢。为卿执杯，逐梦同行。”</p>
 </div>
