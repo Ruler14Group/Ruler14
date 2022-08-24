131e97321bc63f5be90e0eb0dbf8bb4bb0b30bd0
Linmo <30459943+MoorCorPa@users.noreply.github.com>
2022-08-24 18:52:45 +0800

    remake

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '一个为实现梦想而奋斗的Minecraft制作组',
+  description: '~',
+  base: 'Ruler14Group',
+})
--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-const ENV = process.env.NODE_ENV;
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: ENV === "development" ? "" : "/Ruler14Group/"
-})
